36351554aebce2cdd80e139ffb6f6dbeb55d1595
Anida <54995191+anida21@users.noreply.github.com>
2021-04-11 22:44:06 +0200

    commit to 'master'

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.use("/api/users", usersRoutes);
 app.use("/api/posts", postsRoutes);
 
 module.exports = app;
-
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
