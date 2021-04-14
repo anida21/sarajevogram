e3aeddee6d14dee2f3386d7af5ab06eadfd2a79f
Anida Mujezin <anidam@comtrade.com>
2021-04-14 11:37:22 +0200

    port

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ module.exports = app;
 //app.listen(process.env.PORT || 3000, () => {
   //console.log("Server is listening on port 3000");
 //});
-app.listen(process.env.PORT || 3000, function(){
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
